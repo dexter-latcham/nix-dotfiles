ad23d062b310071f08ccb68a52d1f0998819bea6
dexter latcham <flatcapdex@pm.me>
2026-01-03 16:30:52 +0000

    DWM: added more bar padding

--- modules/core/suckless/dwm/config.my.h
+++ modules/core/suckless/dwm/config.my.h
@@ -17,8 +17,8 @@ static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corne
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
-static const int vertpad            = 5;       /* vertical padding of bar */
-static const int sidepad            = 5;       /* horizontal padding of bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 
 
 static const int showbar            = 1;        /* 0 means no bar */
@@ -113,24 +113,12 @@ static const char *termcmd[]  = { TERMINAL, NULL };
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-		{ "normbgcolor",        STRING,  &normbgcolor },
-		{ "normbordercolor",    STRING,  &normbordercolor },
-		{ "normfgcolor",        STRING,  &normfgcolor },
-		{ "selbgcolor",         STRING,  &selbgcolor },
-		{ "selbordercolor",     STRING,  &selbordercolor },
-		{ "selfgcolor",         STRING,  &selfgcolor },
-		{ "borderpx",          	INTEGER, &borderpx },
-
-
-/*
- * can load some settings via xresources
- */
-		{ "snap",          		INTEGER, &snap },
-		{ "showbar",          	INTEGER, &showbar },
-		{ "topbar",          	INTEGER, &topbar },
-		{ "nmaster",          	INTEGER, &nmaster },
-		{ "resizehints",       	INTEGER, &resizehints },
-		{ "mfact",      	 	FLOAT,   &mfact },
+		{ "color0",    STRING,  &normbordercolor },
+		{ "color8",     STRING,  &selbordercolor },
+		{ "color0",        STRING,  &normbgcolor },
+		{ "color4",        STRING,  &normfgcolor },
+		{ "color0",         STRING,  &selfgcolor },
+		{ "color4",         STRING,  &selbgcolor },
 };
 
 
